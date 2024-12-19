7e0a2efe4e533fd9a73fe488d0f985ed26786b56
vijayvj3031 <vijayin.vj@gmail.com>
2024-12-19 18:49:19 +0530

    pushing the latest changes which includes installing packages

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'VJ',
+    author_email= 'vijaykumar.kothawar@gmail.com',
     packages= find_packages(),
     install_requires = []
 
